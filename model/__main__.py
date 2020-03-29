b7c145686bdb16b39258b4282a48bcc7590669f3
danielcpham <dcp5@rice.edu>
2020-03-29 18:38:16 -0500

    added lang model to pipfile, refactored helper funcs

--- model/__main__.py
+++ model/__main__.py
@@ -2,12 +2,12 @@ import argparse
 import logging
 import os
 import sys
-import pdb
 
-import en_core_web_sm
+
 import numpy as np
 import pandas as pd
 import spacy
+import en_core_web_sm
 from textblob import TextBlob
 from tqdm import tqdm
 
@@ -17,35 +17,43 @@ from reappraisal import (Model, SentimentWrapper, extrapolate_data,
                          normalize_sentiment)
 
 # Parse the arguments passed into the command line.
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "-f", "--farAway", help="reappraise using far-away distancing", action='store_true')
 parser.add_argument(
     "-o", "--objective", help="reappraise using objective distancing", action="store_true")
+parser.add_argument(
+    "-d", "--dev", help="Used to print debug statements", action='store_true')
 args = parser.parse_args()
 if not (args.farAway or args.objective):
     parser.exit(
         "Error: Please specify either far-away distancing (-f) or objective distancing (-o)")
 
-# Load english language model.
-nlp = spacy.load("en_core_web_sm")
 
-# Set logger.
-logger = logging.getLogger("MAIN")
+# Load english language model.
+nlp = en_core_web_sm.load()
+
 formatter = logging.Formatter(
     '[%(name)s-%(levelname)s]: %(asctime)-15s: %(message)s')
-# Set logging level of stdout
-logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler(sys.stdout)
 ch.setFormatter(formatter)
-ch.setLevel(logging.INFO)
+if args.dev:
+    ch.setLevel(logging.DEBUG)
+else:
+    ch.setLevel(logging.INFO)
+# Set logger.
+logger = logging.getLogger("MAIN")
+logger.setLevel(logging.DEBUG)
+# Set logging level of stdout
 logger.addHandler(ch)
+
 
 
 def main():
     # Read training data
     cwd = os.getcwd()
-    print(cwd)
     data = pd.DataFrame(
         columns=[['response', 'score_spatiotemp', 'score_obj']])
     for filename in os.listdir(cwd + "/input/training"):
@@ -53,6 +61,13 @@ def main():
             cwd + "/input/training/" + filename).dropna()], axis=0)
         logger.info(f'Added {filename} to training.')
     # Remove invalid data
+    if args.dev:
+        import pdb
+        import faulthandler
+        faulthandler.enable()
+        # Shrink dataset to check
+        data = data.sample(100)
+        logger.debug("Smaller Dataset used.")
     data = data.dropna()
     data.reset_index(drop=True, inplace=True)
 
@@ -61,25 +76,29 @@ def main():
     data_train.to_csv("output/data_train_fixed.csv")
     data_test.to_csv("output/data_test_fixed.csv")
 
-    # Run train -> test on objective distancing
-    if args.objective:
-        correl_obj, _ = run(data_train[['response', 'score_obj']], data_test[[
-            'response', 'score_obj']], nlp, reappStrategyFactory('obj'))
+    with pd.ExcelWriter("output/results.xlsx") as writer:
+        # Run train -> test on objective distancing
+        if args.objective:
+            logger.info("Running algorithm on objective distancing.")
+            correl_obj, test_res_obj = run(data_train[['response', 'score_obj']], data_test[['response', 'score_obj']],
+                                        nlp, reappStrategyFactory('obj'))
+            logger.info(f"Correlation for objective distancing: {correl_obj}")
+            print(test_res_obj)
+            test_res_obj.to_excel(writer, sheet_name='objective', index=False)
+        if args.farAway:
+            # Run train -> test on far away distancing
+            logger.info("Running algorithm on far away distancing.")
+            correl_spatiotemp, test_res_st = run(data_train[['response', 'score_spatiotemp']], data_test[[
+                'response', 'score_spatiotemp']], nlp, reappStrategyFactory('spatiotemp'))
+            logger.info(
+                f"Correlation for Far Away distancing: {correl_spatiotemp}")
+            test_res_st.to_excel(writer, sheet_name='far away',index=False)
 
-        logger.info(f"Correlation for objective distancing: {correl_obj}")
-    if args.farAway:
-        # Run train -> test on far away distancing
-        correl_spatiotemp, _ = run(data_train[['response', 'score_spatiotemp']], data_test[[
-            'response', 'score_spatiotemp']], nlp, reappStrategyFactory('spatiotemp'))
 
-        logger.info(
-            f"Correlation for Far Away distancing: {correl_spatiotemp}")
-
-
-def run(data_train, data_test, nlp, reappStrategy):
+def run(data_train: pd.DataFrame, data_test: pd.DataFrame, nlp, reappStrategy):
 
     # format test data
-    data_test.insert(2, 'observed', [-1.0] * len(data_test))
+    data_test['observed'] = [np.nan] * len(data_test)
 
     # Create reappraisal model and fit training data
     model = Model(nlp, reappStrategy)
@@ -95,12 +114,17 @@ def run(data_train, data_test, nlp, reappStrategy):
             _, score = model.predict(response)
             data_test.at[index, 'observed'] = score
             pbar.update(1)
-    data_test.to_csv(f"output/data_test_results_{reappStrategy.name}.csv")
+    # with open("fuck.csv", "w+") as csv:
+    #     data_test.to_csv(csv, index_label="serial")
+    # data_test.to_csv(index_label="serial")
+    logger.debug("Hello!")
     # if reappStrategy.name == data_process.farAwayFlag:
     #     correl = data_test['observed'].corr(data_test['score_spatiotemp'])
     # elif reappStrategy.name == data_process.objectiveFlag:
     #     correl = data_test['observed'].corr(data_test['score_obj'])
     correl = 0
+    data_test.index.name = 'serial'
+    data_test.reset_index(inplace=True)
     return correl, data_test
 
 
--- model/reappraisal.py
+++ model/reappraisal.py
@@ -1,10 +1,9 @@
 import logging
 import os
+import pdb
 import sys
 from collections import defaultdict
 from dataclasses import dataclass
-from spacy.tokens import Doc
-
 
 import numpy as np
 import pandas as pd
@@ -15,10 +14,10 @@ from spacy.lang.en.stop_words import STOP_WORDS
 from spacy.tokens import Doc, Token
 from textblob import TextBlob
 from tqdm import tqdm
-import pdb
 
-from data_process import (
-    SentimentWrapper, reappStrategyFactory, ObjectiveStrategy, SpatioTempStrategy)
+from utils import (normalize_sentiment, convert_polarity, convert_subj_to_obj, convert_to_wordnet)
+
+from data_process import (ObjectiveStrategy,SpatioTempStrategy, reappStrategyFactory)
 
 FORMAT = '%(asctime)-15s: %(message)s'
 
@@ -310,21 +309,6 @@ def extrapolate_data(filename):
     return df
 
 
-def convert_to_wordnet(tag):
-    """
-    :param tag: POS tag as defined by Penn Treebank
-    :return: POS tag for use in wordnet
-    """
-    if tag in {'NN', 'NNS', "NNP", 'NNPS', "n"}:
-        return 'n'
-    elif tag in {'VB', 'VBD', 'VBP', 'VBZ', 'v'}:
-        return 'v'
-    elif tag in {'a', 'JJ', 'JJR', 'JJS'}:
-        return 'a'
-    elif tag in {'r', 'RB', 'RBR', 'RBS', 'WRB'}:
-        return 'r'
-    else:
-        return None
 
 
 def get_synonyms(sentence, word, tag=None):
@@ -351,25 +335,4 @@ def get_synonyms(sentence, word, tag=None):
     return []
 
 
-def normalize_sentiment(polarity, subjectivity):
-    pol = convert_polarity(polarity)
-    obj = convert_subj_to_obj(subjectivity)
-    return SentimentWrapper(pol, obj)
 
-
-def convert_polarity(pol):
-    # Polarity ranges from [-1, 1] (By TextBlob API). If the polarity has absolute value 1 (-1 or 1),
-    #         then set it to 0.01 to avoid getting a 0 value.
-    #         Else, take the negative and add 1 to get the score between [0, 1].
-    if np.abs(pol) == 1:
-        return 0.01
-    return -np.abs(pol) + 1
-
-
-def convert_subj_to_obj(subj):
-    # Subjectivity ranges from [0, 1] (By Textblob API). Subtract 1 and get absolute value such that
-    #         text closer to 0 have a higher objectivity value.
-    #         Else, leave it.
-    if np.abs(subj) == 1:
-        return 0.01
-    return np.abs(subj - 1)

d4ff10fcd506681896abf3023d0f9c05b7d8372f
Daniel Pham <28274722+danielcpham@users.noreply.github.com>
2021-02-07 16:30:22 -0600

    testing lightning

--- src/ReappModel.py
+++ src/ReappModel.py
@@ -1,35 +1,46 @@
+import torch 
 from torch import nn
+from torch.nn import functional as F
 from transformers import DistilBertForSequenceClassification
 
 
-
 class ReappModel(nn.Module):
-    def __init__(self):
+    def __init__(self) -> None:
         """Generates a sentiment classifier model from a pretrained huggingface model.
         Args:
         """
+        D_in, H, D_out = 768, 50, 7
         super(ReappModel, self).__init__()
-        model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
-
-        self.num_labels = model.config.num_labels
-
-        self.transformer = nn.Sequential(*list(model.children())[:-3])
-        # Freeze sentiment classification since it's already been finetuned
-        self.classifier_head = nn.Sequential(*list(model.children())[-3:])
-        self.classifier_head.requires_grad_(False)
-
-        #Use linear layer to the number of "labels" or "features" to fit to score, and then logsoftmax 
-        self.reapp_scorer = nn.Linear(model.config.num_labels, 1)
-        self.loss = nn.MSELoss()
+        model = DistilBertForSequenceClassification.from_pretrained(
+            "distilbert-base-uncased-finetuned-sst-2-english"
+        )
+        self.bert_encoder = model
 
 
-    def forward(self, input_ids, attention_mask, score, **bert_kwargs):
-        # pooled output applies the activation function on the first token's hidden state
-        # Returns last_hidden_state, hidden_states (opt), attentions (opt)
-        bert_output = self.transformer(
+        # Instantiate an one-layer feed-forward classifier
+        self.classifier = nn.Sequential(
+            nn.Linear(D_in, H),
+            nn.ReLU(),
+            #nn.Dropout(0.5),
+            nn.Linear(H, D_out),
+            nn.ReLU(),
+            nn.Linear(D_out, 8)
+        )
+        # Freeze BERT.
+        # for param in self.bert_encoder.parameters():
+        #     param.requires_grad = False
+
+    def forward(self, input_ids, attention_mask):
+        bert_output = self.bert_encoder(
             input_ids=input_ids,
             attention_mask=attention_mask,
-            **bert_kwargs)
-        predicted
-        loss = self.loss(predicted.float(), score.float().unsqueeze(-1))
-        return loss, bert_output
+            output_hidden_states=True,
+            return_dict=True
+        ) # output is of type SequenceClassifierOutput
+        # Note: Loss here is the regression for sentiment classifier labels (neg, pos)
+        print(bert_output)
+        last_hidden_state = bert_output.hidden_states[-1]
+        
+        # Softmax the logits to get the probability of that label.
+        logits = self.classifier(last_hidden_state)
+        return logits
